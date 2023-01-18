af210881b1e9b9b5e8aa2fc60a17c54f9d9f74b5
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2023-01-18 07:32:58 -0300

    docs: testing lua documentation
    Testing documentation in lua to make methods and tables easier to
    understand.

--- conf/awesome/conf/settings.lua
+++ conf/awesome/conf/settings.lua
@@ -1,11 +1,22 @@
-local settings = {
+---@class Settings
+local settings =
+{
+    ---@type string
     terminal = os.getenv("TERM") or "alacritty",
+    ---@type string
     editor = os.getenv("EDITOR") or "nvim",
+    ---@type string
     filebrowser = os.getenv("FILEBROWSER") or "pcmanfm",
+    ---@type string
     browser = os.getenv("BROWSER") or "firefox",
+    ---@type string
     mail = os.getenv("MAIL") or "thunderbird",
+    ---@type string
     modkey = "Mod4",
+    ---@type string
     altmod = "Mod1",
+    ---@type string
+    editor_cmd = "",
 }
 
 settings.editor_cmd = settings.terminal .. " -e " .. settings.editor;
--- conf/awesome/main/menu.lua
+++ conf/awesome/main/menu.lua
@@ -1,3 +1,8 @@
+--- Menu module
+--- @param settings Settings settings
+--- @param awful awful module
+--- @param hotkeys_popup hotkeys_popup popup module
+--- @return table menu menu functions
 local function myawesomemenu(settings, awful, hotkeys_popup)
     return {
         { "Hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
@@ -8,6 +13,12 @@ local function myawesomemenu(settings, awful, hotkeys_popup)
     }
 end
 
+--- Main menu
+--- @param settings Settings settings
+--- @param awful awful module
+--- @param beautiful beautiful module
+--- @param myawesomemenu table myawesomemenu menu functions
+--- @return table menu menu functions
 local function mymainmenu(settings, awful, beautiful, myawesomemenu)
     return awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
         { "Open Terminal", settings.terminal }
